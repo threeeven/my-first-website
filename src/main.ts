f7ed3ee723369fb470837d3c8248553dec279c8a
lu <1359122023@qq.com>
2026-04-24 11:47:49 +0800

    热力图

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import './assets/main.css';   // 添加这一行
-import 'md-editor-v3/lib/style.css' // <-- 引入编辑器样式
+import 'md-editor-v3/lib/style.css'; // <-- 引入编辑器样式
 import '@vuepic/vue-datepicker/dist/main.css'
 
 const app = createApp(App);
